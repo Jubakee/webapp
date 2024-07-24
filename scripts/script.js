c0301598f443035d9a0bb2376486db8b513fcb65
SJ <jubakee@gmail.com>
2024-07-24 12:56:34 +1200

    Test Options

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,28 @@ document.addEventListener('visibilitychange', () => {
     }
 });
 
+function ensureDocumentIsScrollable() {
+    const isScrollable =
+      document.documentElement.scrollHeight > window.innerHeight;
+   
+    if (!isScrollable) {
+      document.documentElement.style.setProperty(
+        "height",
+        "calc(100vh + 1px)",
+        "important"
+      );
+    }
+  }
+  function preventCollapse() {
+    if (window.scrollY === 0) {
+      window.scrollTo(0, 1);
+    }
+  }
 
+  const scrollableElement = document.querySelector(".shop-container");
+  scrollableElement.addEventListener("touchstart", preventCollapse);
+
+  window.addEventListener("load", ensureDocumentIsScrollable);
 
 //#region Reset Game
 function resetGame() {
